41d348d3a4fcf9f514e51e8d71ded76812e93be8
Victor Miti <victormiti@umusebo.com>
2024-03-23 09:07:51 +0000

    refactor: i18n

--- project/example/apps.py
+++ project/example/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class ExampleConfig(AppConfig):
     name = "project.example"
     label = "example"
-    verbose_name = "Example"
+    verbose_name = _("Example")
